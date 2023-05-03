487334dd1c875ab9e737666ec6ae81d749c730a3
CoolLikeGoose <niken54@mail.ru>
2023-05-03 12:00:26 +0200

    Added php support

--- Oprosnik/oprosnik.js
+++ Oprosnik/oprosnik.js
@@ -6,6 +6,32 @@ function validateForm() {
         return false;
       }
     }
-    return true;
-  }
+    
+    const form = document.querySelector('.piska');
+    const formData = new FormData(form);
+    const selectedRadioButtons = form.querySelectorAll('input[type="radio"]:checked');
+
+    selectedRadioButtons.forEach(button => {
+      formData.append(button.name, button.value);
+    });
+
+    const name = document.querySelector("#name"); 
+    const surname = document.querySelector("#Surname");
+    const email = document.querySelector("#email");
+
+    formData.append("name", name.value);
+    formData.append("surname", surname.value);
+    formData.append("email", email.value);
+
+    xhr.open('POST', 'save-answers.php');
+    xhr.onload = function() {
+    if (xhr.status === 200) {
+      alert('Данные успешно сохранены');
+      form.reset(); // Очищаем форму
+    } else {
+      alert('Произошла ошибка');
+    }
+    };
+    xhr.send(data);
+}
   
